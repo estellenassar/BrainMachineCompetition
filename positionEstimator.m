0f4efef3e31aff5fd066af85124d84386fe30d78
Estelle <35455537+estellenassar@users.noreply.github.com>
2019-03-20 12:25:19 +0000

    minor changes

--- positionEstimator.m
+++ positionEstimator.m
@@ -4,4 +4,5 @@ function [x, y] = positionEstimator(test_data, model)
 
 x = predict(model.x, test_data, K); % what is k?
 y = predict(model.y, test_data, K);
+
 end
--- positionEstimatorTraining.m
+++ positionEstimatorTraining.m
@@ -94,11 +94,10 @@ end
 
 
 % classifies using the four nearest neighbors
-model.x = fitcknn(test_data, beta.X, 'NumNeighbors',4);
-model.y = fitcknn(test_data, beta.Y, 'NumNeighbors',4);
+model.x = fitcknn(velocity.xaxis, beta.X, 'NumNeighbors',4);
+model.y = fitcknn(velocity.yaxis, beta.Y, 'NumNeighbors',4);
 
-% need to break down test_data into different trials or different neurons?
-% need to go over how the test function works
+% no idea what has to go into fitcknn
 
 
 end
